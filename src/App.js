7a832b96fa8b1692557f6ddfaa0b750457f6e69a
SURASAK NONRURK <nongfew240241.1@gmail.com>
2019-09-22 21:53:00 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="bt">
+         <h2 className="tx2">3SA03</h2>
         <WordCard value="Hello" />
         <h1 className="tx">กดปุ่มเพื่อเรียงคำให้ถูกต้อง</h1>
       </div>
